d1deb3ffcb20db90c471bd1e655508f18f8f4d2f
rudakov <rudakov@cmsplanet.ru>
2019-12-27 17:35:29 +0300

    using hooks

--- src/containers/ToDoListContainer/ToDoListContainer.jsx
+++ src/containers/ToDoListContainer/ToDoListContainer.jsx
@@ -1,37 +1,26 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { deleteTodo } from "../../actions/TodosActions";
-import { bindActionCreators } from "redux";
 import ItemContainer from "../ItemContainer/ItemContainer";
 
 import "./ToDoListContainer.css";
 
-class ToDoListContainer extends Component {
-  render() {
-    const { todos, deleteTodo } = this.props;
-    return (
-
-      <div className="ToDoList">
-        {todos.list.map(todo => (
-          <ItemContainer
-            title={todo.title}
-            id={todo.id}
-            key={todo.id}
-            completed={todo.completed}
-            deleteTodo={deleteTodo}
-          />
-        ))}
-      </div>
-    );
-  }
+const ToDoListContainer = () => {
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
+  return (
+    <div className="ToDoList">
+      {todos.list.map(todo => (
+        <ItemContainer
+          title={todo.title}
+          id={todo.id}
+          key={todo.id}
+          completed={todo.completed}
+          deleteTodo={() => dispatch(deleteTodo(todo.id))}
+        />
+      ))}
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
-  return { todos: state.todos };
-}
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ deleteTodo }, dispatch);
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(ToDoListContainer);
+export default ToDoListContainer;
